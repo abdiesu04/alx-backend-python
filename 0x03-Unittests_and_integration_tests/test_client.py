9df31dc3a84ee6422bc7683e21b688696d858590
abdiesu04 <abdiesayasbayisa@gmail.com>
2024-12-13 14:37:22 +0300

    Add shebang to test_client.py and format imports

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import unittest
 from unittest.mock import patch
+
 from parameterized import parameterized
+
 from client import GithubOrgClient
+
 
 class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
