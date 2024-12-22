9a134f48c9a1ed496cbcd4220e4adbbff5106f0b
abdiesu04 <abdiesayasbayisa@gmail.com>
2024-12-22 22:04:10 +0300

    Add password field to User model in messaging app

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -7,6 +7,7 @@ class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=150, null=False)
     last_name = models.CharField(max_length=150, null=False)
+    password = models.CharField(max_length=128, null=False)
     email = models.EmailField(unique=True, null=False)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     ROLE_CHOICES = [
