b6c92affedb9b97017f49902cd0c1134297127ba
abdiesu04 <abdiesayasbayisa@gmail.com>
2024-12-23 22:57:16 +0300

    Add Conversation and Message viewsets with custom create logic

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+	queryset = Conversation.objects.all()
+	serializer_class = ConversationSerializer
+
+	def create(self, request, *args, **kwargs):
+		# Custom logic for creating a new conversation
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		self.perform_create(serializer)
+		headers = self.get_success_headers(serializer.data)
+		return Response(serializer.data, status=201, headers=headers)
+
+class MessageViewSet(viewsets.ModelViewSet):
+	queryset = Message.objects.all()
+	serializer_class = MessageSerializer
+
+	def create(self, request, *args, **kwargs):
+		# Custom logic for sending a message to an existing conversation
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		self.perform_create(serializer)
+		headers = self.get_success_headers(serializer.data)
+		return Response(serializer.data, status=201, headers=headers)
