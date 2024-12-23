1aab03a5d69f19db076cf1e2a172f31843e8f500
abdiesu04 <abdiesayasbayisa@gmail.com>
2024-12-23 23:12:38 +0300

    Add URL routing for Conversation and Message viewsets with custom create logic

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -7,7 +7,7 @@ class ConversationViewSet(viewsets.ModelViewSet):
 	queryset = Conversation.objects.all()
 	serializer_class = ConversationSerializer
 
-	def create(self, request, *args, **kwargs):
+	def create(self, request):
 		# Custom logic for creating a new conversation
 		serializer = self.get_serializer(data=request.data)
 		serializer.is_valid(raise_exception=True)
